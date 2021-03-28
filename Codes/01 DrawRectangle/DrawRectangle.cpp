f3771f1ef9d444e9c6a8847d5e6de406d8e93adf
Kim DaeSeong <kimds5344@naver.com>
2021-03-28 23:37:43 +0900

    Game Programming

---
+++ Codes/01 DrawRectangle/DrawRectangle.cpp
@@ -0,0 +1,286 @@
+#include "DrawRectangle.h"
+
+// 응용 프로그램의 진입점 함수.
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
+{
+	if (SUCCEEDED(CoInitialize(NULL)))
+	{
+		{
+			DemoApp app;
+			if (SUCCEEDED(app.Initialize(hInstance)))
+			{
+				app.RunMessageLoop();
+			}
+		}
+		CoUninitialize();
+	}
+	return 0;
+}
+
+// 생성자. 멤버 변수들을 초기화함.
+DemoApp::DemoApp() :
+	m_hwnd(NULL),
+	m_pDirect2dFactory(NULL),
+	m_pRenderTarget(NULL),
+	m_pLightSlateGrayBrush(NULL),
+	m_pCornflowerBlueBrush(NULL)
+{
+}
+
+// 소멸자. 응용 프로그램의 자원을 반납함.
+DemoApp::~DemoApp()
+{
+	DiscardDeviceResources();
+	SAFE_RELEASE(m_pDirect2dFactory);
+}
+
+// 응용 프로그램의 원도우를 생성함. 장치 독립적 자원을 생성함.
+HRESULT DemoApp::Initialize(HINSTANCE hInstance)
+{
+	HRESULT hr;
+
+	// 장치 독립적 자원을 생성함.
+	hr = CreateDeviceIndependentResources();
+
+	if (SUCCEEDED(hr))
+	{
+		// 윈도우 클래스를 등록함..
+		WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = DemoApp::WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = sizeof(LONG_PTR);
+		wcex.hInstance = hInstance;
+		wcex.hbrBackground = NULL;
+		wcex.lpszMenuName = NULL;
+		wcex.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+		wcex.lpszClassName = L"D2DDemoApp";
+		RegisterClassEx(&wcex);
+
+		// 윈도우를 생성함.
+		m_hwnd = CreateWindow(
+			L"D2DDemoApp", L"Direct2D Demo Application",
+			WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+			640, 480, NULL, NULL, hInstance, this
+		);
+		hr = m_hwnd ? S_OK : E_FAIL;
+		if (SUCCEEDED(hr))
+		{
+			ShowWindow(m_hwnd, SW_SHOWNORMAL);
+			UpdateWindow(m_hwnd);
+		}
+	}
+
+	return hr;
+}
+
+// 장치 독립적 자원들을 생성함. 이들 자원의 수명은 응용 프로그램이 종료되기 전까지 유효함.
+HRESULT DemoApp::CreateDeviceIndependentResources()
+{
+	HRESULT hr = S_OK;
+
+	// D2D 팩토리를 생성함.
+	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+
+	return hr;
+}
+
+
+// 장치 의존적 자원들을 생성함. 장치가 소실되는 경우에는 이들 자원을 다시 생성해야 함.
+HRESULT DemoApp::CreateDeviceResources()
+{
+	HRESULT hr = S_OK;
+
+	if (!m_pRenderTarget)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+
+		// D2D 렌더타겟을 생성함.
+		hr = m_pDirect2dFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(m_hwnd, size), &m_pRenderTarget);
+
+		if (SUCCEEDED(hr))
+		{
+			// 회색 붓을 생성함.
+			hr = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightSlateGray), &m_pLightSlateGrayBrush);
+		}
+		if (SUCCEEDED(hr))
+		{
+			// 파란색 붓을 생성함.
+			hr = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::CornflowerBlue), &m_pCornflowerBlueBrush);
+		}
+	}
+
+	return hr;
+}
+
+// 장치 의존적 자원들을 반납함. 장치가 소실되면 이들 자원을 다시 생성해야 함.
+void DemoApp::DiscardDeviceResources()
+{
+	SAFE_RELEASE(m_pRenderTarget);
+	SAFE_RELEASE(m_pLightSlateGrayBrush);
+	SAFE_RELEASE(m_pCornflowerBlueBrush);
+}
+
+// 메인 윈도우 메시지 루프를 실행함.
+void DemoApp::RunMessageLoop()
+{
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
+// 클라이언트 윈도우 화면을 그림.
+// 참고: 이 함수는 실행되는 동안에 장치가 소실되면 장치 의존적 자원들을 반납함. 그 다음 호출 시에 그 자원들을 다시 생성함.
+HRESULT DemoApp::OnRender()
+{
+	HRESULT hr = S_OK;
+
+	hr = CreateDeviceResources();
+
+	if (SUCCEEDED(hr))
+	{
+		m_pRenderTarget->BeginDraw();
+		m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+		m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+		D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
+
+		// 격자 배경을 그림.
+		int width = static_cast<int>(rtSize.width);
+		int height = static_cast<int>(rtSize.height);
+
+		for (int x = 0; x < width; x += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
+				D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
+				m_pLightSlateGrayBrush, 0.5f
+			);
+		}
+
+		for (int y = 0; y < height; y += 10)
+		{
+			m_pRenderTarget->DrawLine(
+				D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
+				D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
+				m_pLightSlateGrayBrush, 0.5f
+			);
+		}
+
+		// 두 개의 사각형을 그림.
+		D2D1_RECT_F rectangle1 = D2D1::RectF(
+			rtSize.width / 2 - 50.0f, rtSize.height / 2 - 50.0f,
+			rtSize.width / 2 + 50.0f, rtSize.height / 2 + 50.0f
+		);
+
+		D2D1_RECT_F rectangle2 = D2D1::RectF(
+			rtSize.width / 2 - 100.0f, rtSize.height / 2 - 100.0f,
+			rtSize.width / 2 + 100.0f, rtSize.height / 2 + 100.0f
+		);
+
+
+		// 채워진 사각형을 그림.
+		m_pRenderTarget->FillRectangle(&rectangle1, m_pLightSlateGrayBrush);
+
+		// 채우지 않은 사각형을 그림.
+		m_pRenderTarget->DrawRectangle(&rectangle2, m_pCornflowerBlueBrush);
+
+		hr = m_pRenderTarget->EndDraw();
+	}
+
+	if (hr == D2DERR_RECREATE_TARGET)
+	{
+		hr = S_OK;
+		DiscardDeviceResources();
+	}
+
+	return hr;
+}
+
+
+// 응용 프로그램에서 WM_SIZE 메시지가 발생되면 이 함수가 렌더타겟의 크기를 다시 설정함.
+void DemoApp::OnResize(UINT width, UINT height)
+{
+	if (m_pRenderTarget)
+	{
+		// 참고: 아래의 함수는 실행 시에 실패할 수도 있지만, 여기에서 실행 에러를 체크하지 않고 넘어가도 됨. 다음번 EndDraw 함수가 호출될 때에 에러가 확인될 것이므로 그때 처리하면 됨.
+		m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+	}
+}
+
+// 윈도우 메시지를 처리.
+LRESULT CALLBACK DemoApp::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT result = 0;
+
+	if (message == WM_CREATE)
+	{
+		LPCREATESTRUCT pcs = (LPCREATESTRUCT)lParam;
+		DemoApp* pDemoApp = (DemoApp*)pcs->lpCreateParams;
+
+		SetWindowLongPtrW(hwnd, GWLP_USERDATA, PtrToUlong(pDemoApp));
+
+		result = 1;
+	}
+	else
+	{
+		DemoApp* pDemoApp = reinterpret_cast<DemoApp*>(static_cast<LONG_PTR>(GetWindowLongPtrW(hwnd, GWLP_USERDATA)));
+
+		bool wasHandled = false;
+
+		if (pDemoApp)
+		{
+			switch (message)
+			{
+			case WM_SIZE:
+			{
+				UINT width = LOWORD(lParam);
+				UINT height = HIWORD(lParam);
+				pDemoApp->OnResize(width, height);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DISPLAYCHANGE:
+			{
+				InvalidateRect(hwnd, NULL, FALSE);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_PAINT:
+			{
+				pDemoApp->OnRender();
+				ValidateRect(hwnd, NULL);
+			}
+			result = 0;
+			wasHandled = true;
+			break;
+
+			case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+			}
+			result = 1;
+			wasHandled = true;
+			break;
+			}
+		}
+
+		if (!wasHandled)
+		{
+			result = DefWindowProc(hwnd, message, wParam, lParam);
+		}
+	}
+
+	return result;
+}
